ef39f53b3206b3e6889accc992fb220125047a80
TimothyG77 <timo.gregorian@gmail.com>
2025-03-20 15:50:45 +0100

    Added header.php

---
+++ Frontend/sites/index.php
@@ -0,0 +1,24 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start(); 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Startseite</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="styles.css"> 
+
+</head>
+<body>
+    <?php include 'header.php';?>
+    
+</body>
+</html>
