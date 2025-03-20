69914b22ceaa1ba3583429573bedb159abe613b6
TimothyG77 <timo.gregorian@gmail.com>
2025-03-20 16:25:18 +0100

    update footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="bg-dark text-light py-3 mt-5">
+    <div class="container text-center">
+        <p>&copy; <?php echo date("Y"); ?> SmashPoint. Alle Rechte vorbehalten.</p>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="agb.php" class="text-light">AGB</a></li>
+            <li class="list-inline-item">|</li>
+            <li class="list-inline-item"><a href="datenschutz.php" class="text-light">Datenschutz</a></li>
+            <li class="list-inline-item">|</li>
+            <li class="list-inline-item"><a href="nutzungsbedingungen.php" class="text-light">Nutzungsbedingungen</a></li>
+        </ul>
+    </div>
+</footer>
